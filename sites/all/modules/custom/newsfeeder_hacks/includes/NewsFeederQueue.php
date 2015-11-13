c600c517a36d9563ca1a0a044ccce7bd140dc1fe
Dezső BICZÓ <mxr576@gmail.com>
2015-11-13 08:40:46 +0100

    Custom queue processor added which make it sure, that one queue item is only added one time to the queues.

---
+++ sites/all/modules/custom/newsfeeder_hacks/includes/NewsFeederQueue.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Custom queue implementation for Newsfeeder.
+ *
+ * It makes sure that each queue item exists only once in the queue.
+ */
+class NewsFeederQueue extends SystemQueue {
+  /**
+   * {@inheritdoc}
+   */
+  public function createItem($data) {
+    $serialized_data = serialize($data);
+    // Make sure that, if a queue item already exists, then the existing one
+    // will be updated rather than a new queue item is created.
+    return db_merge('queue')
+      ->key(['name' => $this->name, 'data' => $serialized_data])
+      ->fields([
+        'name' => $this->name,
+        'data' => $serialized_data,
+        'created' => time(),
+      ])->execute();
+  }
+}
